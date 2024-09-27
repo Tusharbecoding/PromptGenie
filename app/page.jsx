806442dc009c6f694522150d4b7082c2b035d700
Tusharbecoding <tusharchopra02122002@gmail.com>
2024-09-28 01:27:21 +0530

    deployment steps

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,15 @@
 import Feed from "@components/Feed";
 
 const Home = () => (
-  <section className='w-full flex-center flex-col'>
-    <h1 className='head_text text-center'>
-      Discover & Share
-      <br className='max-md:hidden' />
-      <span className='orange_gradient text-center'> AI-Powered Prompts</span>
+  <section className="w-full flex-center flex-col">
+    <h1 className="head_text text-center">
+      Generate & Share
+      <br className="max-md:hidden" />
+      <span className="blue_gradient text-center">AI-Crafted Prompts</span>
     </h1>
-    <p className='desc text-center'>
-      Promptopia is an open-source AI prompting tool for modern world to
-      discover, create and share creative prompts
+    <p className="desc text-center">
+      PromptGenie is an open-source AI tool for discovering, creating, and
+      sharing innovative prompts. Elevate your creativity effortlessly.
     </p>
 
     <Feed />
